8cc8cfc6b33220d340979dc77038b9e9083f97d1
Brandon Turner <brandon_turner@rapid7.com>
2015-04-06 13:07:25 -0500

    Bump version for next development iteration

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'ryan_hass@rapid7.com'
 license          'Apache 2.0'
 description      'Installs/Configures jetbrains_license_server'
 long_description 'Installs/Configures jetbrains_license_server'
-version          '0.1.1'
+version          '0.1.2'
 
 depends 'apt',    '>= 2.6.0'
 depends 'java',   '>= 1.29.0'
